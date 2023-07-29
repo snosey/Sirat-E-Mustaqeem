763d8549a896b2523aae4af1e516c93ccbbbfa56
Ahmed El Snosey <ahmed.el.snosey@gmail.com>
2023-07-29 12:11:49 +0300

    update

--- lib/src/core/util/controller/location_controller.dart
+++ lib/src/core/util/controller/location_controller.dart
@@ -8,7 +8,6 @@ import 'package:geolocator/geolocator.dart';
 import '../../error/error_code.dart';
 import '../../error/exceptions.dart';
 import '../../error/failures.dart';
-import '../../keys.dart';
 import '../../network/api_service.dart';
 import '../../network/network_client.dart';
 import '../model/geocoding.dart';
@@ -105,7 +104,7 @@ Future<Either<Failure, Geocoding>> getAddress(
   // / query parameters for get request to get address from api
   Map<String, Object> params = {
     'latlng': '$latitude,$longitude',
-    'key': kGoogleCloudApiKey,
+    'key': "kGoogleCloudApiKey", // TODO change to your own api key
   };
 
   try {
